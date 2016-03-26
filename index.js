9427921b24fdfc770f690183e54d462b738aae84
Lydia van Deursen <lvdeursen@virtual-affairs.com>
2016-03-26 15:40:27 +0100

    easter egg

---
+++ index.js
@@ -0,0 +1,16 @@
+//module.exports = function(width, height) {  
+//  return width * height;
+//};
+
+console.log("");
+console.log("EASTER EGG");
+console.log("");
+console.log("What day does an Easter egg hate the most?");
+console.log("");
+console.log("");
+console.log("Fry-days");
+console.log("");
+console.log(":-)");
+
+
+ 
